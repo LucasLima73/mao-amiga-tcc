4c4a767173dca40c1dc2e227c58483fc2648f264
Lucas Lima <lucaslavey@outlook.com>
2024-12-21 14:19:26 -0300

    add todolist

--- app/(components)/(contentlayout)/layout.js
+++ app/(components)/(contentlayout)/layout.js
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import Backtotop from '../../../shared/layout-components/backtotop/backtotop'
 import Footer from '../../../shared/layout-components/footer/footer'
 import Header from '../../../shared/layout-components/header/header'
@@ -6,27 +6,24 @@ import Sidebar from '../../../shared/layout-components/sidebar/sidebar'
 import Switcher from '../../../shared/layout-components/switcher/switcher'
 import React, { useEffect, useState } from 'react'
 
-const Layout = ({children}) => {
-    const [lateLoad, setlateLoad] = useState(false);
-    useEffect(() => {
-      setlateLoad(true);
-    });
+const Layout = ({ children }) => {
+  const [lateLoad, setlateLoad] = useState(false)
+  useEffect(() => {
+    setlateLoad(true)
+  })
   return (
     <>
-      <div style={{display: `${lateLoad ? "block" : "none"}`}}>
-        <Switcher/>
-        <div className='page'>
-            <Header/>
-            <Sidebar/>
-            <div className='main-content app-content'>
-              <div className='container-fluid'>
-                  {children}
-              </div>
-            </div>
-            <Footer/>
+      <div style={{ display: `${lateLoad ? 'block' : 'none'}` }}>
+        <Switcher />
+        <div className="page">
+          <Header />
+          {/* <Sidebar/>*/}
+          <div className="main-content app-content">
+            <div className="container-fluid">{children}</div>
+          </div>
+          <Footer />
         </div>
-        <Backtotop/>
-    </div>
+      </div>
     </>
   )
 }
