468e6125b67e69f4c4f1d932fed02def1504c08d
Lucas Lima <lucaslavey@outlook.com>
2025-01-09 20:08:44 -0300

    ajustes e adição de paginas

--- shared/services/openAiConfig.js
+++ shared/services/openAiConfig.js
@@ -1,32 +1,33 @@
 import OpenAI from 'openai'
 
-
-
 // Função para gerar embeddings
 export const createEmbedding = async (content) => {
-    const maxRetries = 3;
-    let attempt = 0;
-  
-    while (attempt < maxRetries) {
-      try {
-        const response = await openai.embeddings.create({
-          model: "text-embedding-ada-002",
-          input: content,
-        });
-  
-        return response.data[0].embedding;
-      } catch (error) {
-        if (error.response && error.response.status === 429) {
-          console.warn(`Rate limit atingido. Tentando novamente (${attempt + 1}/${maxRetries})...`);
-          attempt++;
-          await new Promise((resolve) => setTimeout(resolve, 2000 * attempt)); // Aguardar antes de tentar novamente
-        } else {
-          console.error("Erro ao criar embedding:", error);
-          throw new Error("Falha ao gerar embedding.");
-        }
+  const maxRetries = 3
+  let attempt = 0
+
+  while (attempt < maxRetries) {
+    try {
+      const response = await openai.embeddings.create({
+        model: 'text-embedding-ada-002',
+        input: content,
+      })
+
+      return response.data[0].embedding
+    } catch (error) {
+      if (error.response && error.response.status === 429) {
+        console.warn(
+          `Rate limit atingido. Tentando novamente (${
+            attempt + 1
+          }/${maxRetries})...`
+        )
+        attempt++
+        await new Promise((resolve) => setTimeout(resolve, 2000 * attempt)) // Aguardar antes de tentar novamente
+      } else {
+        console.error('Erro ao criar embedding:', error)
+        throw new Error('Falha ao gerar embedding.')
       }
     }
-  
-    throw new Error("Limite de tentativas excedido ao gerar embedding.");
-  };
-  
+  }
+
+  throw new Error('Limite de tentativas excedido ao gerar embedding.')
+}
